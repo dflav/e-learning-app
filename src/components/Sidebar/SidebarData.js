adecac0cf578823572ae5d84d4a3fabcacff0029
dflav <flavio.dollani@gmail.com>
2022-10-04 18:19:31 +0300

    Fixed Routes

--- src/components/Sidebar/SidebarData.js
+++ src/components/Sidebar/SidebarData.js
@@ -15,10 +15,10 @@ const SidebarData = [
     dropdownClose: <RiArrowDownSLine />,
     dropdownOpen: <RiArrowUpSLine />,
     subNav: [
-      { id: 2, title: 'Κεφάλαιο 1', path: '/lessons/IT1', icon: <IoIosPaper /> },
-      { id: 3, title: 'Κεφάλαιο 2', path: '/lessons/IT2', icon: <IoIosPaper /> },
-      { id: 4, title: 'Κεφάλαιο 3', path: '/lessons/IT3', icon: <IoIosPaper /> },
-      { id: 5, title: 'Κεφάλαιο 4', path: '/lessons/IT4', icon: <IoIosPaper /> },
+      { id: 2, title: 'Κεφάλαιο 1', path: '/e-learning-app/lessons/IT1', icon: <IoIosPaper /> },
+      { id: 3, title: 'Κεφάλαιο 2', path: '/e-learning-app/lessons/IT2', icon: <IoIosPaper /> },
+      { id: 4, title: 'Κεφάλαιο 3', path: '/e-learning-app/lessons/IT3', icon: <IoIosPaper /> },
+      { id: 5, title: 'Κεφάλαιο 4', path: '/e-learning-app/lessons/IT4', icon: <IoIosPaper /> },
     ],
   },
   {
@@ -29,15 +29,15 @@ const SidebarData = [
     dropdownClose: <RiArrowDownSLine />,
     dropdownOpen: <RiArrowUpSLine />,
     subNav: [
-      { id: 7, title: 'Κεφάλαιο 1', path: '/quiz/IT1', icon: <HiOutlinePencilAlt /> },
-      { id: 8, title: 'Κεφάλαιο 2', path: '/quiz/IT2', icon: <HiOutlinePencilAlt /> },
-      { id: 9, title: 'Κεφάλαιο 3', path: '/quiz/IT3', icon: <HiOutlinePencilAlt /> },
-      { id: 10, title: 'Κεφάλαιο 4', path: '/quiz/IT4', icon: <HiOutlinePencilAlt /> },
-      { id: 11, title: 'Επαναληπτικό', path: '/quiz/rev', icon: <HiOutlinePencilAlt /> },
+      { id: 7, title: 'Κεφάλαιο 1', path: '/e-learning-app/quiz/IT1', icon: <HiOutlinePencilAlt /> },
+      { id: 8, title: 'Κεφάλαιο 2', path: '/e-learning-app/quiz/IT2', icon: <HiOutlinePencilAlt /> },
+      { id: 9, title: 'Κεφάλαιο 3', path: '/e-learning-app/quiz/IT3', icon: <HiOutlinePencilAlt /> },
+      { id: 10, title: 'Κεφάλαιο 4', path: '/e-learning-app/quiz/IT4', icon: <HiOutlinePencilAlt /> },
+      { id: 11, title: 'Επαναληπτικό', path: '/e-learning-app/quiz/rev', icon: <HiOutlinePencilAlt /> },
     ],
   },
-  { id: 12, title: 'Προφίλ', path: '/profile', icon: <CgProfile /> },
-  { id: 13, title: 'Εγχειρίδιο', path: '/guide', icon: <AiOutlineInfoCircle /> },
+  { id: 12, title: 'Προφίλ', path: '/e-learning-app/profile', icon: <CgProfile /> },
+  { id: 13, title: 'Εγχειρίδιο', path: '/e-learning-app/guide', icon: <AiOutlineInfoCircle /> },
 ]
 
 export default SidebarData
